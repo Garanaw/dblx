aa5a4f462f6db572aca98a05357e0305913e002b
Pablo Villalba <garanaw@gmail.com>
2020-11-26 18:11:38 +0000

    Adds scaffold for authentication

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,7 @@ $router = app(Router::class);
 $router->get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
